4a89d7918dfc09b630a4dcf7ccd4cc436cd09d69
Favna <jeroen.claassens@live.nl>
2016-08-07 03:39:17 +0200

    Add icon and userscript

---
+++ KATMagneto.User.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         KATMagneto
+// @version      1.0
+// @description  Make the Magnet button work again!
+// @author       Favna
+// @license http://www.apache.org/licenses/LICENSE-2.0
+// @icon https://raw.github.com/favna/KATMagneto/master/assets/icon.png
+// @homepage https://github.com/Favna/KATMagneto
+// @twitterURL https://twitter.com/Favna_
+// @contactURL https://www.reddit.com/message/compose/?to=Favna
+// @supportURL https://github.com/Favna/KATMagneto/issues
+// @match        http://kickasstorrentsan.com/*
+// ==/UserScript==
+var magnet = $("[data-sc-params]").attr("data-sc-params");
+magnet = magnet.replace("{ 'magnet': '","");
+magnet = magnet.split("'")[0];
+$(".kaGiantButton").attr("href", magnet);
