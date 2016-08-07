33caaa1a714d4711c9a587bad661cdee010196f5
Favna <jeroen.claassens@live.nl>
2016-08-07 04:35:25 +0200

    further cleanup and adding @grant

--- KATMagneto.js
+++ KATMagneto.js
@@ -1,25 +1,21 @@
 // ==UserScript==
-// @name         KATMagneto
-// @version      1.0
-// @description  Make the Magnet button work again!
-// @author       Favna
-// @license http://www.apache.org/licenses/LICENSE-2.0
-// @icon https://raw.githubusercontent.com/Favna/KATMagneto/master/Assets/icon.png
-// @homepage https://github.com/Favna/KATMagneto
-// @twitterURL https://twitter.com/Favna_
-// @contactURL https://www.reddit.com/message/compose/?to=Favna
-// @supportURL https://github.com/Favna/KATMagneto/issues
-// @updateURL https://raw.githubusercontent.com/Favna/KATMagneto/master/KATMagneto.js
-// @downloadURL https://raw.githubusercontent.com/Favna/KATMagneto/master/KATMagneto.js
-// @domain kickasstorrentsan.com
-// @match        http://kickasstorrentsan.com/*
+// @name			KATMagneto
+// @version			1.0
+// @description		Make the Magnet button work again!
+// @author			Favna
+// @license			http://www.apache.org/licenses/LICENSE-2.0
+// @icon			https://raw.githubusercontent.com/Favna/KATMagneto/master/Assets/icon.png
+// @homepage		https://github.com/Favna/KATMagneto
+// @twitterURL		https://twitter.com/Favna_
+// @contactURL		https://www.reddit.com/message/compose/?to=Favna
+// @supportURL		https://github.com/Favna/KATMagneto/issues
+// @updateURL		https://raw.githubusercontent.com/Favna/KATMagneto/master/KATMagneto.js
+// @downloadURL		https://raw.githubusercontent.com/Favna/KATMagneto/master/KATMagneto.js
+// @domain			kickasstorrentsan.com
+// @match			http://kickasstorrentsan.com/*
+// @grant			none
 // ==/UserScript==
-
-(function() {
-    'use strict';
-
 var magnet = $("[data-sc-params]").attr("data-sc-params");
-magnet = magnet.replace("{ 'magnet': '","");
+magnet = magnet.replace("{ 'magnet': '", "");
 magnet = magnet.split("'")[0];
 $(".kaGiantButton").attr("href", magnet);
-})();
