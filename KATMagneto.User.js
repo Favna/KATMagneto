2727bb1ae845f8409c993c1771578543318af9ee
Favna <jeroen.claassens@live.nl>
2016-08-07 03:43:49 +0200

    Update userscript variables to have proper URL's

--- KATMagneto.User.js
+++ KATMagneto.User.js
@@ -4,11 +4,13 @@
 // @description  Make the Magnet button work again!
 // @author       Favna
 // @license http://www.apache.org/licenses/LICENSE-2.0
-// @icon https://raw.github.com/favna/KATMagneto/master/assets/icon.png
+// @icon https://raw.githubusercontent.com/Favna/KATMagneto/master/Assets/icon.png
 // @homepage https://github.com/Favna/KATMagneto
 // @twitterURL https://twitter.com/Favna_
 // @contactURL https://www.reddit.com/message/compose/?to=Favna
 // @supportURL https://github.com/Favna/KATMagneto/issues
+// @updateURL https://raw.githubusercontent.com/Favna/KATMagneto/master/KATMagneto.User.js
+// @downloadURL https://raw.githubusercontent.com/Favna/KATMagneto/master/KATMagneto.User.js
 // @match        http://kickasstorrentsan.com/*
 // ==/UserScript==
 var magnet = $("[data-sc-params]").attr("data-sc-params");
