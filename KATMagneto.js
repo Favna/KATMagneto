c36907a0593dac615cb3d993115eed766d597bbf
Favna <jeroen.claassens@live.nl>
2016-08-07 04:45:40 +0200

    trying to fix downloading still

--- KATMagneto.js
+++ KATMagneto.js
@@ -13,7 +13,23 @@
 // @downloadURL		https://raw.githubusercontent.com/Favna/KATMagneto/master/KATMagneto.js
 // @domain			kickasstorrentsan.com
 // @match			http://kickasstorrentsan.com/*
-// @grant			none
+// @grant			unsafeWindow
+// @grant			GM_addStyle
+// @grant			GM_getValue
+// @grant			GM_setValue
+// @grant			GM_xmlhttpRequest
+// @grant			GM_registerMenuCommand
+// @grant			GM_deleteValue
+// @grant			GM_listValues
+// @grant			GM_getResourceText
+// @grant			GM_getResourceURL
+// @grant			GM_log
+// @grant			GM_openInTab
+// @grant			GM_setClipboard
+// @grant			GM_info
+// @grant			GM_getMetadata
+// @run-at			document-start
+// @connect *
 // ==/UserScript==
 var magnet = $("[data-sc-params]").attr("data-sc-params");
 magnet = magnet.replace("{ 'magnet': '", "");
