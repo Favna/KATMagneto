360e4d95f7cc682f5e7d4cd82359b7c5f4ccd82d
Favna <jeroen.claassens@live.nl>
2016-08-07 03:57:14 +0200

    forgot to add file and edit readme

---
+++ KATMagneto-userscript.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         KATMagneto
+// @version      1.0
+// @description  Make the Magnet button work again!
+// @author       Favna
+// @license http://www.apache.org/licenses/LICENSE-2.0
+// @icon https://raw.githubusercontent.com/Favna/KATMagneto/master/Assets/icon.png
+// @homepage https://github.com/Favna/KATMagneto
+// @twitterURL https://twitter.com/Favna_
+// @contactURL https://www.reddit.com/message/compose/?to=Favna
+// @supportURL https://github.com/Favna/KATMagneto/issues
+// @updateURL https://raw.githubusercontent.com/Favna/KATMagneto/master/KATMagneto-userscript.js
+// @downloadURL https://raw.githubusercontent.com/Favna/KATMagneto/master/KATMagneto-userscript.js
+// @domain kickasstorrentsan.com
+// @match        http://kickasstorrentsan.com/*
+// ==/UserScript==
+use strict;
+
+var magnet = $("[data-sc-params]").attr("data-sc-params");
+magnet = magnet.replace("{ 'magnet': '","");
+magnet = magnet.split("'")[0];
+$(".kaGiantButton").attr("href", magnet);
