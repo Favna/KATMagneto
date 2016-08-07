d6912da5a69eda6f74fae8f312b2d61776dc0d7d
Favna <jeroen.claassens@live.nl>
2016-08-07 05:03:30 +0200

    finally got automatic install working & also cleaning up stuff. Hopefully all done now

--- KATMagneto.user.js
+++ KATMagneto.user.js
@@ -13,25 +13,16 @@
 // @updateURL https://raw.githubusercontent.com/Favna/KATMagneto/master/KATMagneto.js
 // @downloadURL https://raw.githubusercontent.com/Favna/KATMagneto/master/KATMagneto.js
 // @installURL https://raw.githubusercontent.com/Favna/KATMagneto/master/KATMagneto.js
-// @domain kickasstorrentsan.com
-// @match http://kickasstorrentsan.com/*
 // @grant unsafeWindow
-// @grant GM_addStyle
 // @grant GM_getValue
 // @grant GM_setValue
-// @grant GM_xmlhttpRequest
-// @grant GM_registerMenuCommand
 // @grant GM_deleteValue
 // @grant GM_listValues
 // @grant GM_getResourceText
 // @grant GM_getResourceURL
-// @grant GM_log
 // @grant GM_openInTab
 // @grant GM_setClipboard
-// @grant GM_info
 // @grant GM_getMetadata
-// @run-at document-start
-// @connect *
 // ==/UserScript==
 var magnet = $("[data-sc-params]").attr("data-sc-params");
 magnet = magnet.replace("{ 'magnet': '", "");
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # KATMagneto
 KATMagneto - A browser userscript to make the magnet button on Kickasstorrents work without account
 
-Download the userscript [by clicking here](https://raw.githubusercontent.com/Favna/KATMagneto/master/KATMagneto.js)
+Download the userscript [by clicking here](https://raw.githubusercontent.com/Favna/KATMagneto/master/KATMagneto.user.js)
